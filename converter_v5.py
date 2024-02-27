a95d551fa4e71eedba03c40c32d2ec350c1d4455
kimfjellbo <36606642+kimfjellbo@users.noreply.github.com>
2024-02-27 10:25:11 +0100

    Add files via upload

---
+++ converter_v5.py
@@ -0,0 +1,176 @@
+#!/usr/bin/python3
+import sys
+import re
+import csv
+
+if len(sys.argv) < 2:
+    sys.exit("Need 2 arguments, zzz.py {file}")
+else:
+    file1 = sys.argv[1]
+    file2 = sys.argv[2]
+
+zzzLines = []
+dic = dict()
+dicTable = dict()
+
+splitChar = "|X|"
+
+with open(file1, 'r') as zzz_all_zzz:
+    lineNumber = 1
+    for line in zzz_all_zzz:
+        dic[lineNumber] = line.strip()
+        job = re.search('(ZZZ)',line)
+        if job:
+            zzzLines.append(lineNumber)
+        lineNumber = lineNumber + 1
+
+# generic method to check if the index in a array exist
+
+def checkNext(arr,number):
+    try:
+        arr[number]
+        return True
+    except:
+        return False
+
+# Create dictionary with the splitChar between the values
+
+for iter in range (1,len(zzzLines)):
+    if ( checkNext(zzzLines,1) ):
+        temp1 = zzzLines[0]
+        temp2 = zzzLines[1]
+        zzzLines.pop(0)
+        appendLines = []
+        for n in range(temp1,temp2):
+            appendLines.append(dic[n])
+        string = splitChar.join(appendLines)
+        dicTable[iter] = string
+
+
+# splitSplit() that returns the zzz name and value and tests
+
+def splitSplit(text, splitChar, textSearch):
+    splitted = text.split(splitChar)
+    attr = ""
+    val = ""
+    for valueSplitted in splitted:
+        if ( re.search('(ZZZ)',textSearch) ):
+            attr = valueSplitted[0:19].strip()
+            val = valueSplitted[19:len(valueSplitted)].strip()
+            break
+        
+        indexColumnInText = valueSplitted.strip().find(":")
+        attributeName = valueSplitted.strip()[0:indexColumnInText]
+        value = valueSplitted.strip()[indexColumnInText+1:len(valueSplitted)]
+        if( attributeName.strip() == textSearch.strip() ):
+            attr = attributeName.strip()
+            val = value.strip()
+            break
+    return attr, val
+
+
+def setAllAttributesAndValues(fileout):
+    arr = dict()
+    with open(f"{fileout}", 'w', newline='', encoding='utf-8') as csv_file:
+        fieldnames = [
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+			'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            'zzz',
+            ]
+        csvWriter = csv.DictWriter(csv_file, fieldnames=fieldnames, delimiter=";")
+        csvWriter.writeheader()
+        for iter in dicTable:
+            lineSplit = dicTable[iter].split(splitChar)
+            ATTRIBUTES_job = re.search('(zzz)',lineSplit[0])
+            if( ATTRIBUTES_job ):
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+                zzz = splitSplit(dicTable[iter], splitChar, "zzz")[1]
+
+                csvWriter.writerow(
+                    {
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz,
+                    'zzz':zzz
+                    }
+                    )
+
+setAllAttributesAndValues(file2)
---
+++ helperScriptPostnumber-ver2.py
@@ -0,0 +1,60 @@
+# Works with Python 3.9.6
+import sys
+import codecs
+import re
+import string
+
+arg1_rec1 = sys.argv[1]
+arg2_pops = sys.argv[2]
+
+print("command argument 1 is: "+arg1_rec1)
+print("command argument 2 is: "+arg2_pops)
+
+regexValues = {"rec1File": '(\d{5}\w+\s{0,1}-{0,1}\w+)', "postnumber": '(\d{5})', "address": '(\D+\w+\s{0,1}\w+)'}
+
+# read and parse the pops file
+
+filePostnumberPops = codecs.open(arg2_pops, mode='r', encoding='utf-8', errors='strict')
+
+zzzdbArray = []
+for line in filePostnumberPops.readlines():
+    zzzdbArray.append(line.strip().upper())
+
+distinctList1 = set(zzzdbArray)
+
+# read and parse the sweden postnumber file
+
+filePostnumberSweden = codecs.open(arg1_rec1, mode='r', encoding='utf-8', errors='strict')
+
+postnumberrec1File = []
+for line in filePostnumberSweden.readlines():
+    regularexp = re.search(regexValues["rec1File"], line)
+    postnumberrec1File.append(regularexp.group(1).upper())
+
+distinctList2 = set(postnumberrec1File)
+
+# find differences
+
+missingPostnumbersInPops = distinctList2 - distinctList1
+obsoletePostnumbersInPops = distinctList1 - distinctList2
+
+for val in missingPostnumbersInPops:
+    postnumber = re.search(regexValues["postnumber"], val)
+    address = re.search(regexValues["address"], val)
+    try:
+        temp = string.Template('INSERT INTO TPostNumber(PostNumber_Number, PostNumber_Name, PostNumber_Country) values (\'$postnumber\',\'$address\',\'SE\')')
+        insertIntoSQL = temp.substitute(postnumber=postnumber.group(1), address=address.group(1))
+        print(insertIntoSQL)
+    except AttributeError:
+        temp = string.Template('The value "$val" cannot be parsed with regex "$regExPostnumber" and/or $regExAdress')
+        print(temp.substitute(val=val, regExPostnumber=regexValues["postnumber"], regExAdress=regexValues["address"]))
+
+for val in obsoletePostnumbersInPops:
+    postnumber = re.search(regexValues["postnumber"], val)
+    try:
+        temp = string.Template('SELECT PostNumber_Id,PostNumber_Number,PostNumber_Name FROM TPostNumber with(nolock) WHERE PostNumber_Number = \'$postnumber\' and PostNumber_Country = \'SE\'')
+        selectSQL = temp.substitute(postnumber=postnumber.group(1))
+        print(selectSQL)
+    except AttributeError:
+        temp = string.Template('The value "$val" cannot be parsed with regex "$regExPostnumber"')
+        print(temp.substitute(val=val, regExPostnumber=regexValues["postnumber"]))
